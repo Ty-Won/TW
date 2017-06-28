91254f29da62672e7f70a7fd55aa3a1ba8ca96f6
TyWon96 <tyronewong96@gmail.com>
2017-06-28 00:20:27 -0400

    Bootstrap

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,17 +1,19 @@
-document.addEventListener("DOMContentLoaded",main, false);
+document.addEventListener("DOMContentLoaded", main, false);
 
-function main(){
-    $(document).ready(function(){
-      $('.slider').slider();
-    });
+function main() {
+  $(document).ready(function () {
+    $('.materialboxed').materialbox();
+  });
 
-    
-// Pause slider
-$('.slider').slider('pause');
-// Start slider
-$('.slider').slider('start');
-// Next slide
-$('.slider').slider('next');
-// Previous slide
-$('.slider').slider('prev');
+
+  // Pause slider
+  $('.slider').slider('pause');
+  // Start slider
+  $('.slider').slider('start');
+  // Next slide
+  $('.slider').slider('next');
+  // Previous slide
+  $('.slider').slider('prev');
 }
+
+
