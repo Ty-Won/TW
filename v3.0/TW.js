3c928928b7fa63e74f6727c97bf94975ad9b856e
TyWon96 <tyronewong96@gmail.com>
2017-07-12 23:42:20 -0400

    Cleaned up javascript files and arranged the about and jumbotron

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,19 +1,2 @@
-document.addEventListener("DOMContentLoaded", main, false);
-
-function main() {
-  $(document).ready(function () {
-    $('.materialboxed').materialbox();
-  });
-
-
-  // Pause slider
-  $('.slider').slider('pause');
-  // Start slider
-  $('.slider').slider('start');
-  // Next slide
-  $('.slider').slider('next');
-  // Previous slide
-  $('.slider').slider('prev');
-}
 
 
