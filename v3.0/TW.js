34ff6449fd0fa2250544438fa3838d342341aebb
TyWon96 <tyronewong96@gmail.com>
2017-07-21 22:40:05 -0400

    Html+ css syntax cleanup and removal of projects (beta)

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,4 +1,14 @@
 
 /*darkens specified elements when hovered over*/
+$(".descent").hover(function(){
+  $(this).addClass("descent-hover");
+  $('.fa-chevron-down').addClass("fa-chevron-down-color");
+  console.log("hello");
+})
 
 
+$("descent").hover(function(){
+    $(this).css("background-color", "yellow");
+    }, function(){
+    $(this).css("background-color", "pink");
+});
