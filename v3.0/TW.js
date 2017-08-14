7bfef74c0e31293a8c8a14f7aa4b2278a8cad587
TyWon96 <tyronewong96@gmail.com>
2017-08-13 23:44:41 -0400

    menu scroll effect

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,29 +1,42 @@
 
 $(document).ready(function () {
+
   $(".jumbotron").hide();
   $(".jumbotron").fadeIn(2000);
   $(".descent")
-  .mouseover(function () {
-    $(this).addClass("descent-hover");
-    $('.fa-chevron-down').addClass("fa-chevron-down-color");
-  })
-  .mouseout(function() {
-    $(this).removeClass("descent-hover");
-    $('.fa-chevron-down').removeClass("fa-chevron-down-color");
+    .mouseover(function () {
+      $(this).addClass("descent-hover");
+      $('.fa-chevron-down').addClass("fa-chevron-down-color");
+    })
+    .mouseout(function () {
+      $(this).removeClass("descent-hover");
+      $('.fa-chevron-down').removeClass("fa-chevron-down-color");
 
+    });
+
+
+  //Modal js in development
+  // $("#contact-button").onClick(function(){
+  //   $("#contact-modal").fadeIn(300);
+  // });
+
+  // $(window).onClick(function(event){
+  //   var contactModal=$("#contact-modal");
+  //   if(event.target==contactModal){
+  //     contactModal.css("display:block;")
+  //   };
+  // });
+
+  $('main').scroll(function () {
+    if ($('main').scrollTop() > 50) {
+      $('.mdl-layout__header.mdl-layout__header--transparent').css({ "background-color": "lightgrey !important" });
+      console.log("yo");
+    } else {
+      $('.mdl-layout__header.mdl-layout__header--transparent').css({ "background-color": "transparent" });
+      console.log("yoo");
+    }
   });
-
-  
-  $("#contact-button").onClick(function(){
-    $("#contact-modal").fadeIn(300);
-  });
-
-  $(window).onClick(function(event){
-    var contactModal=$("#contact-modal");
-    if(event.target==contactModal){
-      contactModal.css("display:block;")
-    };
-  });
-
-
 });
+
+
+
