a14c02e8c21f3f635c68db9fb73a187810172c21
TyWon96 <tyronewong96@gmail.com>
2017-08-23 23:32:36 -0400

    Rework, switch to boostrap beta

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,30 +1,30 @@
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  $(".jumbotron").hide();
-  $(".jumbotron").fadeIn(2000);
-  $(".descent")
-    .mouseover(function () {
-      $(this).addClass("descent-hover");
-      $('.fa-chevron-down').addClass("fa-chevron-down-color");
-    })
-    .mouseout(function () {
-      $(this).removeClass("descent-hover");
-      $('.fa-chevron-down').removeClass("fa-chevron-down-color");
+//   $(".jumbotron").hide();
+//   $(".jumbotron").fadeIn(2000);
+//   $(".descent")
+//     .mouseover(function () {
+//       $(this).addClass("descent-hover");
+//       $('.fa-chevron-down').addClass("fa-chevron-down-color");
+//     })
+//     .mouseout(function () {
+//       $(this).removeClass("descent-hover");
+//       $('.fa-chevron-down').removeClass("fa-chevron-down-color");
 
-    });
-
-
+//     });
 
 
-  $('main').scroll(function () {
-    if ($('main').scrollTop() > 750) {
-      $('header').addClass("visible-navbar");
-    } else {
-      $('header').removeClass("visible-navbar");
-    }
-  });
-});
+
+
+//   $('main').scroll(function () {
+//     if ($('main').scrollTop() > 750) {
+//       $('header').addClass("visible-navbar");
+//     } else {
+//       $('header').removeClass("visible-navbar");
+//     }
+//   });
+// });
 
 
 
