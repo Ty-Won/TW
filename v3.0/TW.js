33d408c258c0482acecaaf7683ca881774517cac
TyWon96 <tyronewong96@gmail.com>
2017-09-11 23:04:40 -0400

    Fixed script for modal and descent button

--- v3.0/TW.js
+++ v3.0/TW.js
@@ -1,30 +1,44 @@
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//   $(".jumbotron").hide();
-//   $(".jumbotron").fadeIn(2000);
-//   $(".descent")
-//     .mouseover(function () {
-//       $(this).addClass("descent-hover");
-//       $('.fa-chevron-down').addClass("fa-chevron-down-color");
-//     })
-//     .mouseout(function () {
-//       $(this).removeClass("descent-hover");
-//       $('.fa-chevron-down').removeClass("fa-chevron-down-color");
+    /*Descent button glowing animation*/
+    $(".descent")
+        .mouseover(function () {
+            $(this).addClass("descent-hover");
+            $('.fa-chevron-down').addClass("fa-chevron-down-color");
+        })
+        .mouseout(function () {
+            $(this).removeClass("descent-hover");
+            $('.fa-chevron-down').removeClass("fa-chevron-down-color");
 
-//     });
-
+        });
 
 
+    /*navbar transparency function that changes the color of the navbar once it scrolls past a certain point*/
+    $('main').scroll(function () {
+        if ($('main').scrollTop() > 750) {
+            $('header').addClass("visible-navbar");
+        } else {
+            $('header').removeClass("visible-navbar");
+        }
+    });
 
-//   $('main').scroll(function () {
-//     if ($('main').scrollTop() > 750) {
-//       $('header').addClass("visible-navbar");
-//     } else {
-//       $('header').removeClass("visible-navbar");
-//     }
-//   });
-// });
+    /*Modal function for the contact button, hides menu and background while the modal is up and allows the modal
+    to be closed when clicked outside of the window*/
+    $("#contact-button").click(function () {
+        $('.mdl-layout__obfuscator').removeClass('is-visible');
+        $('.mdl-layout__drawer').removeClass('is-visible');
+
+
+    });
+
+    $(window).click(function (event) {
+        var contactModal = $("#contact-modal");
+        if (event.target == contactModal) {
+            contactModal.hide();
+        };
+    });
+});
 
 
 
